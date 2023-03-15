79e04d00dfe6b9d4fa5a611f78c10764a2b15b3c
Alyaa Awny <alyaaawny6@gmail.com>
2023-03-15 22:13:11 +0200

    First Task

---
+++ Lab1/Controllers/CarsController.cs
@@ -0,0 +1,33 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1.Controllers
+{
+    public enum Status
+    {
+        list,
+        table
+
+    }
+    public class CarDetails
+    {
+        public Car? car { get; set; }
+        public Status? status { get; set; }
+    }
+    public class CarsController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            var cars = Car.GetCars();
+            return View(cars);//here you send model to view now you need to write the model inside the view
+        }
+        public IActionResult GetDetails(string carModel, Status? status)
+        {
+            var car = Car.GetCars().First(c => c.Model == carModel);
+            CarDetails details = new CarDetails();
+            details.car = car;
+            details.status = status;
+            return View(details);
+        }
+    }
+}
---
+++ Lab1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region services 
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        #endregion
+        #region Actions based on endpoint url one of these functions will be called 
+        public IActionResult Index()
+        {//you can write any message here to appeare in console window 
+           // _logger.LogInformation("Hello Guys I'm Alyaa Awny a new developer ");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public string HelloDeveloper() {
+            _logger.LogInformation("hello message");
+            return "hello developer I am Alyaa Awny And I am a newcomer here!"; }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        #endregion
+    }
+}
